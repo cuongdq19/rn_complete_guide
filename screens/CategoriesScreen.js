9eb4cacf65431332dae2c34d7f25bc5cc2e544a6
Cuong Dang <cuong.developer19@gmail.com>
2021-09-27 15:22:51 +0700

    End of section 6

---
+++ screens/CategoriesScreen.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import { StyleSheet, FlatList } from 'react-native';
+import { HeaderButtons, Item } from 'react-navigation-header-buttons';
+
+import { CATEGORIES } from '../data/dummy-data';
+import CategoryGridTile from '../components/CategoryGridTile';
+import HeaderButton from '../components/HeaderButton';
+
+const CategoriesScreen = (props) => {
+  const renderGridItem = (itemData) => {
+    return (
+      <CategoryGridTile
+        title={itemData.item.title}
+        color={itemData.item.color}
+        onSelect={() => {
+          props.navigation.navigate({
+            routeName: 'CategoryMeals',
+            params: {
+              categoryId: itemData.item.id,
+            },
+          });
+        }}
+      />
+    );
+  };
+
+  return (
+    <FlatList
+      keyExtractor={(item) => item.id}
+      data={CATEGORIES}
+      renderItem={renderGridItem}
+      numColumns={2}
+    />
+  );
+};
+
+CategoriesScreen.navigationOptions = (navigationData) => {
+  return {
+    headerTitle: 'Meals Categories',
+    headerLeft: (
+      <HeaderButtons HeaderButtonComponent={HeaderButton}>
+        <Item
+          title="Menu"
+          iconName="ios-menu"
+          onPress={() => {
+            navigationData.navigation.toggleDrawer();
+          }}
+        />
+      </HeaderButtons>
+    ),
+  };
+};
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
+
+export default CategoriesScreen;
---
+++ screens/FiltersScreen.js
@@ -0,0 +1,118 @@
+import React, { useState, useEffect, useCallback } from 'react';
+import { StyleSheet, Text, View, Switch, Platform } from 'react-native';
+import { HeaderButtons, Item } from 'react-navigation-header-buttons';
+
+import HeaderButton from '../components/HeaderButton';
+import Colors from '../constants/Colors';
+
+const FilterSwitch = (props) => {
+  return (
+    <View style={styles.filterContainer}>
+      <Text>{props.label}</Text>
+      <Switch
+        trackColor={{ true: Colors.primaryColor }}
+        thumbColor={Platform.OS === 'android' ? Colors.primaryColor : ''}
+        value={props.value}
+        onValueChange={props.onChange}
+      />
+    </View>
+  );
+};
+
+const FiltersScreen = (props) => {
+  const [isGlutenFree, setIsGlutenFree] = useState(false);
+  const [isLactoseFree, setIsLactoseFree] = useState(false);
+  const [isVegan, setIsVegan] = useState(false);
+  const [isVegetarian, setIsVegetarian] = useState(false);
+
+  const { navigation } = props;
+
+  const saveFilters = useCallback(() => {
+    const appliedFilters = {
+      glutenFree: isGlutenFree,
+      lactoseFree: isLactoseFree,
+      vegan: isVegan,
+      vegetarian: isVegetarian,
+    };
+
+    console.log(appliedFilters);
+  }, [isGlutenFree, isLactoseFree, isVegan, isVegetarian]);
+
+  useEffect(() => {
+    navigation.setParams({ save: saveFilters });
+  }, [saveFilters]);
+
+  return (
+    <View style={styles.screen}>
+      <Text style={styles.title}>Available Filters / Restrictions</Text>
+      <FilterSwitch
+        label="Gluten-free"
+        value={isGlutenFree}
+        onChange={(value) => setIsGlutenFree(value)}
+      />
+      <FilterSwitch
+        label="Lactose-free"
+        value={isLactoseFree}
+        onChange={(value) => setIsLactoseFree(value)}
+      />
+      <FilterSwitch
+        label="Vegan"
+        value={isVegan}
+        onChange={(value) => setIsVegan(value)}
+      />
+      <FilterSwitch
+        label="Vegetarian"
+        value={isVegetarian}
+        onChange={(value) => setIsVegetarian(value)}
+      />
+    </View>
+  );
+};
+
+FiltersScreen.navigationOptions = (navigationData) => {
+  return {
+    headerTitle: 'Filter Meals',
+    headerLeft: (
+      <HeaderButtons HeaderButtonComponent={HeaderButton}>
+        <Item
+          title="Menu"
+          iconName="ios-menu"
+          onPress={() => {
+            navigationData.navigation.toggleDrawer();
+          }}
+        />
+      </HeaderButtons>
+    ),
+    headerRight: (
+      <HeaderButtons HeaderButtonComponent={HeaderButton}>
+        <Item
+          title="Save"
+          iconName="ios-save"
+          onPress={navigationData.navigation.getParam('save')}
+        />
+      </HeaderButtons>
+    ),
+  };
+};
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    alignItems: 'center',
+  },
+  title: {
+    fontFamily: 'open-sans-bold',
+    fontSize: 22,
+    margin: 20,
+    textAlign: 'center',
+  },
+  filterContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginVertical: 15,
+    width: '80%',
+  },
+});
+
+export default FiltersScreen;
